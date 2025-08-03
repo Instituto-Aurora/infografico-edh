b92e316ce76b1ff639fefce9d25ea35417d315b7
ericakn <erica.k.nishimura@gmail.com>
2025-08-03 10:39:25 -0300

    fix(airtable): maxrecords 200

--- pages/api/airtable.ts
+++ pages/api/airtable.ts
@@ -57,7 +57,7 @@ function getEndpoint(tableName: string, lng: string | string[]) {
   const tableId =
     env !== "production" ? AIRTABLE_STG_VIEW_ID : AIRTABLE_PROD_VIEW_ID;
 
-  return `https://api.airtable.com/v0/${tableId}/${tableName}__${lng}?maxRecords=100&view=Grid%20view`;
+  return `https://api.airtable.com/v0/${tableId}/${tableName}__${lng}?maxRecords=200&view=Grid%20view`;
 }
 
 export default async function handler(
