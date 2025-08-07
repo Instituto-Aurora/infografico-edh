c48cc6d2d432773ddb9f2f5d11f897e55d2defdd
ericakn <erica.k.nishimura@gmail.com>
2025-08-06 23:54:01 -0300

    feat(envvar): add airtable id via vercel variables

--- shared/utils/constants.ts
+++ shared/utils/constants.ts
@@ -4,7 +4,6 @@ export const IS_IN_MAINTENANCE: boolean =
 export const env: string =
   process.env.NEXT_PUBLIC_VERCEL_ENV || process.env.NODE_ENV || "development";
 
-//export const AIRTABLE_PROD_VIEW_ID = "app8KjMHegoxFRjgU";
-export const AIRTABLE_PROD_VIEW_ID = "appkzRAtPnCzMdeKB";
+export const AIRTABLE_PROD_VIEW_ID = "app8KjMHegoxFRjgU";
 
 export const AIRTABLE_STG_VIEW_ID = "appkzRAtPnCzMdeKB";
